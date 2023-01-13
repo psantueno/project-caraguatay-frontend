2361d0b8ce46e4d73ab4000ebb872d0907f02946
unknown <aracelicatalano@gmail.com>
2023-01-13 14:56:59 -0300

    formulario Crear noticia creado con bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';  
 import { Home } from './Home';
+import Formulario  from './Formulario';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export const App = () => {
@@ -9,6 +11,7 @@ export const App = () => {
     <div>
       <Routes>
         <Route exact path="/" element={<Home/>} ></Route>
+        <Route exact path="/formulario" element={<Formulario />} ></Route>
       </Routes>      
     </div>
   )
