d13374558c64349c865ef6be52c72cfff459fb9b
Leandro Martin, Balado Vidal <lm.baladovidal@gmail.com>
2023-01-18 17:26:59 -0300

    se modifico la forma en al que se muestra el menu y se agrego un breakpoint a los 780 px

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-  import React from 'react'
+import React from 'react'
 import { Menu } from './Menu'
 import { ProfileButton } from './ProfileButton'
 import escudo from '../assets/escudo.png'
@@ -7,12 +7,14 @@ import './Header.css'
   export const Header = () => {
     return (
         <>
-        <div className='main-div-header'>
-            <ProfileButton/>
-            <img src={escudo} className="escudo"/>
-            <Menu/>            
-        </div>
-        <hr></hr>
+          <div className='main-div-header'>
+          <div>
+              <ProfileButton/>
+              <img src={escudo} className="escudo"/>
+          </div>
+              <Menu/>            
+          </div>
+          <hr/>
         </>
     )
   }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,28 +7,27 @@ export const Menu = () => {
 
     const openNav = (event)=>{
         document.getElementById("mySidenav").style.width = "250px";        
-    }
-    
+    }    
     const closeNav =(event)=> {
         document.getElementById("mySidenav").style.width = "0";
-    }
-    
+    }    
     const [menu,setMenu] = useState(false)
-        const toggleMenu = ()=>{
+    const toggleMenu = ()=>{
             setMenu(!menu)
         }
 
   return (
     <>
-    <div id="mySidenav" className={`sidenav ${menu?'is-active':''}`} onClick={toggleMenu}>
-        <a href="#">About<hr/></a>
-        <a href="#">Services<hr/></a>
-        <a href="#">Clients<hr/></a>
-        <a href="#">Contact<hr/></a>
-    </div>
-    <div id="main">
-        <span className="span-menu" onClick={toggleMenu}>&#9776;</span>
-    </div>
+        <div id="mySidenav" className={`sidenav ${menu?'is-active':''}`} onClick={toggleMenu}>
+            <a href="#">Comunicados<hr/></a>
+            <a href="#">Deportes<hr/></a>
+            <a href="#">Cultura y Turismo<hr/></a>
+            <a href="#">Historia<hr/></a>
+            <a href="#">Punto Digital<hr/></a>
+        </div>
+        <div id="main" className='sidenav-button'>
+            <span className="span-menu" onClick={toggleMenu}>&#9776;</span>
+        </div>
     </>
   )
 }
