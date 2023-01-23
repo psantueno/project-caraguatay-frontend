136146d0520d7dd03e6874a38a94f3f6443f7005
psantueno <seba.antueno@gmail.com>
2023-01-23 13:23:25 -0300

    finished footer

---
+++ src/PoweredBy.jsx
@@ -0,0 +1,15 @@
+
+export const PoweredBy = () => {
+
+    return (
+        <div className="powered-by">
+            <h6 className="menu-subtitle"><b>Desarrollado por:</b></h6>
+            <ul className="list-section">
+                <li>Sebastián Antueno</li>
+                <li>Araceli Catalano</li>
+                <li>Alba Morán</li>
+                <li>Leandro Balado Vidal</li>
+            </ul>
+        </div>
+    )
+}
