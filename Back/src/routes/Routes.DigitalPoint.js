189a4b020b20c603c92a3e49a75162748d307f31
psantueno <seba.antueno@gmail.com>
2023-07-25 21:12:23 -0300

    limpieza del main

--- Back/src/routes/Routes.DigitalPoint.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {methods as digitalPoint} from '../controllers/Controller.digital_points';
-const router = Router();
-router.get('list/all',digitalPoint.listAllDigitalPoint);
-router.get('/list/:idDigitalPoint',digitalPoint.listDigitalPoint);
-router.put('/update/:idDigitalPoint',digitalPoint.updateDigitalPoint);
-router.delete('delete/:idDigitalPoint',digitalPoint.deleteDigitalPoint);
-router.post('/create/',digitalPoint.createDigitalPoint);
-export default router;
