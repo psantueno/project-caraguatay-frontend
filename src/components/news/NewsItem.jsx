e5dfd153ad0742da050ef92fdc57c075c42c4d6d
Alba <alba.morann@gmail.com>
2023-08-26 16:18:49 -0300

    tarjeta de news muestra imagenes de la tabla media

--- src/components/news/NewsItem.jsx
+++ src/components/news/NewsItem.jsx
@@ -1,46 +1,34 @@
 import React, { useState } from 'react'
 import { Card, Container, Collapse } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import photo1 from '../../assets/images/news.jpg'
+// import photo1 from '../../assets/images/news.jpg'
 import '../../index.css';
 
 
 export const NewsItem = ({
-   
 
-    photo = { url: photo1 },
-    category = {category},
-    date = {date},
-    title = {title},
-    text = {mainText},
+    photo = { url },
+    category = { category },
+    date = { date },
+    title = { title },
+    text = { mainText },
     // link = {link}
 
 }) => {
     const [open, setOpen] = useState(false);
     const [textCollpase, setTextCollapse] = useState({})
 
-    const textCollapsed=  () => {
-        
+    const textCollapsed = () => {
+
         setOpen(!open),
-        setTextCollapse(textCollpase)
-        
-        
-        }
-
-
-
-
-       
-        
-
-
-
+            setTextCollapse(textCollpase)
+    }
 
     return (
         <>
             <Container className="container-news" >
                 <Card className='cardNews' >
-                    <Card.Img variant="top" src={photo1} className='cardImg' />
+                    <Card.Img variant="top" src={photo} className='cardImg' />
                     <Card.Subtitle className="cardSubtitle">
                         <Card.Text className="cardCategory">
                             <small> <Link to="#" className="cardLink" > {category} </Link></small>
@@ -52,31 +40,19 @@ export const NewsItem = ({
                     <Card.Body className='cardBody'>
                         <Card.Title className='cardTitle'>{title}</Card.Title>
                         <Card.Text className='cardText' >
-                                {
-                                    <>
-                            
-
-                            { text.length < 100  ? {text} : 
-                            <>
-                             `${text.slice(0, 100)}...` 
-                            </>
-                            }
-                             
-                                                                                
+                            {
+                                <>
+                                    {text.length < 100 ? { text } :
+                                        <>
+                                            `${text.slice(0, 100)}...`
+                                        </>
+                                    }
                                     <button onClick={textCollapsed}>
-                                    {open ? 'Leer menos...' : 'Leer más...'}
+                                        {open ? 'Leer menos...' : 'Leer más...'}
                                     </button>
-                                
-                            </>
+                                </>
                             }
-                            
 
-
-
-
-
-
-                            
                         </Card.Text>
                     </Card.Body>
                     <Collapse in={open}>
--- src/components/news/NewsItemContainer.jsx
+++ src/components/news/NewsItemContainer.jsx
@@ -3,37 +3,33 @@ import { useFetchNewsCategories } from "../../hooks/useFetchNewsCategories";
 import { NewsItem } from "./NewsItem";
 
 
-export const NewsItemContainer = () =>{
+export const NewsItemContainer = () => {
 
-const { news } = useFetchNews();
+    const { news } = useFetchNews();
 
+    return (
+        <>
+            {
+                news.map((news, index) => (
 
+                    <div key={index}>
 
+                        <NewsItem
+                            // category = {news.newsCategory_id === categoryNews.id ? categoryNews.category : null}
+                            category={news.category}
+                            date={news.date}
+                            title={news.title}
+                            text={news.mainText}
+                            photo={news.url}
 
-return(
-    <>
-    {
-        news.map((news, index) => (
-    
-    <div key={index}>
+                        />
 
-        <NewsItem 
-        // category = {news.newsCategory_id === categoryNews.id ? categoryNews.category : null}
-        category ={news.category }
-        date = {news.date}
-        title = {news.title}
-        text = {news.mainText}
-      
-            
-            
-            />
+                    </div>
 
-    </div>
-
-        
-      ))
-    }
-    </>
-)}
+                ))
+            }
+        </>
+    )
+}
 
 
