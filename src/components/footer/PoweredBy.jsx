f2c18ebe39bed1bd4da142ad0d1f0aa41181ccba
Alba <alba.morann@gmail.com>
2024-02-04 11:52:54 -0300

    fix css on Footer section

--- src/components/footer/PoweredBy.jsx
+++ src/components/footer/PoweredBy.jsx
@@ -4,12 +4,12 @@ export const PoweredBy = () => {
     return (
         <div className="powered-by">
             <h6 className="menu-subtitle"><b>Desarrollado por:</b></h6>
-            <ul className="list-section">
+            {/* <ul className="list-section">
                 <li>Sebastián Antueno</li>
                 <li>Araceli Catalano</li>
                 <li>Alba Morán</li>
-                <li>Leandro Balado Vidal</li>
-            </ul>
+            </ul> */}
+            <span className="list-section"> Sebastián Antueno - Araceli Catalano - Alba J. Morán</span>
         </div>
     )
 }
